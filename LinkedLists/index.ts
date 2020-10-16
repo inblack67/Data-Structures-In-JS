cdcfeb97932f0c576bdd02682aa69b96c93cffdc
Aman Bhardwaj <devproddeploy@gmail.com>
2020-10-16 21:07:49 +0530

    queues

--- LinkedLists/index.ts
+++
@@ -1,9 +0,0 @@
-const node1 = {
-    data: 10,
-}
-const node2 = {
-    data: 20,
-    next: null
-}
-node1.next = node2;
-console.log(node1)
---
+++ Queues/index.js
@@ -0,0 +1,24 @@
+class Queue {
+	constructor() {
+		this.elements = [];
+		this.size = this.elements.length;
+	}
+	enqueue(el) {
+		this.elements.unshift(el);
+	}
+	dequeue() {
+		this.elements.shift();
+	}
+	display() {
+		console.log(this.elements);
+	}
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
+queue.dequeue();
+queue.display();
