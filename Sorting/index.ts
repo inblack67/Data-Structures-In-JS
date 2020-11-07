df50b18c80416e30efbd5e2ba083d05db376cde4
Aman Bhardwaj <devproddeploy@gmail.com>
2020-11-07 10:22:17 +0530

    selection sort

--- Sorting/index.ts
+++ Sorting/index.ts
@@ -30,9 +30,47 @@ class Sort
             }
         }
     }
+    insertionSort ()
+    {
+        for ( let i = 1; i < arr.length; i++ )
+        {
+            let j = i - 1;
+            let x = arr[ i ];
+            while ( arr[ j ] > x && j > -1 )
+            {
+                arr[ j + 1 ] = arr[ j ];
+                j--;
+            }
+
+            arr[ j + 1 ] = x;
+        }
+    }
+    selectionSort ()
+    {
+        for ( let i = 0; i < this.arr.length; i++ )
+        {
+            let k = i;
+            let j = i;
+            for ( ; j < this.arr.length; j++ )
+            {
+                if ( this.arr[ j ] < this.arr[ k ] )
+                {
+                    this.swap( j, k );
+                }
+            }
+        }
+    }
+    swap ( x: number, y: number )
+    {
+        let temp = this.arr[ x ];
+        this.arr[ x ] = this.arr[ y ];
+        this.arr[ y ] = temp;
+    }
 }
 
 const arr = [ 8, 5, 7, 3, 2 ];
 const s1 = new Sort( arr );
-s1.bubbleSort();
+// s1.bubbleSort();
+// s1.insertionSort();
+s1.selectionSort();
 console.log( s1.arr );
